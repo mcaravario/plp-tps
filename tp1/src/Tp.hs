513e59b53874e67b0e385b7690c20fdd9254c654
Martin <martin.caravario@gmail.com>
2016-04-06 16:38:31 -0300

    Más correcciones de estilo

--- tp1/src/Tp.hs
+++ tp1/src/Tp.hs
@@ -21,31 +21,38 @@ mean :: [Float] -> Float
 mean xs = realToFrac (sum xs) / genericLength xs
 
 split :: Eq a => a -> [a] -> [[a]]
-split c = foldl (\acum x -> if x == c then acum++[[]] else (init acum)++[(last acum)++[x]]) [[]]
+split c yss = filter null (split' c yss)
+  where split' c xss = foldr (\x rec -> if x==c then agregarVacia rec else agregarAlPrimero x rec) [[]] xss
+        agregarVacia xss = [] : xss
+        agregarAlPrimero x xss = (x : head xss) : tail xss
 
 longitudPromedioPalabras :: Extractor
-longitudPromedioPalabras t = mean $ map (genericLength) (split ' ' t)
+longitudPromedioPalabras texto = mean $ map genericLength palabrasSeparadas
+  where palabrasSeparadas = split ' ' texto
 
 apariciones :: Eq a => a -> [a] -> Int
-apariciones e xs = length $ filter (==e) xs
+apariciones elem xs = length $ filter (==elem) xs
 
 cuentas :: Eq a => [a] -> [(Int, a)]
-cuentas xs = map (\y -> (apariciones y xs, y)) (nub xs)
+cuentas xs = map cantidadApariciones (nub xs)
+  where cantidadApariciones = \y -> (apariciones y xs, y)
 
 repeticionesPromedio :: Extractor
-repeticionesPromedio xs = mean $ map (fromIntegral.fst) (cuentas (split ' ' xs))
+repeticionesPromedio xs = mean $ map cantidad palabrasContadas 
+  where cantidad = fromIntegral . fst
+        palabrasContadas = cuentas (split ' ' xs)
 
 tokens :: [Char]
 tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
 
 frecuenciaTokens :: [Extractor]
 frecuenciaTokens = map frecuencia tokens
-	where frecuencia t xs = fromIntegral (apariciones t xs) / genericLength xs
+  where frecuencia token texto = fromIntegral (apariciones token texto) / genericLength texto 
 
 normalizarExtractor :: [Texto] -> Extractor -> Extractor
-normalizarExtractor ts e = (\xs -> e xs / maximo)
-	where maximo = maximum $ map (abs.e) ts
-
+normalizarExtractor textos extractor = \xs -> extractor xs / maximo
+  where maximo = maximum $ map (abs . extractor) textos
+              
 extraerFeatures :: [Extractor] -> [Texto] -> Datos
 extraerFeatures es ts = let normalizados = map (normalizarExtractor ts) es in
 	map (evaluar normalizados) ts
@@ -63,25 +70,32 @@ distEuclideana xs ys = norm $ zipWith (-) xs ys
 distCoseno :: Medida
 distCoseno xs ys = (prodInt xs ys) / (norm xs * norm ys)
 
-calcular_max_moda :: [(Float,Etiqueta)] -> Etiqueta
-calcular_max_moda ls = snd (maximumBy (compare `on` fst) (cuentas [snd x | x <- ls]))
+calcularEtiquetaModa :: [(Float,Etiqueta)] -> Etiqueta
+calcularEtiquetaModa ls = snd (maximumBy compararPorPrimero cantidadEtiquetas) 
+  where cantidadEtiquetas = cuentas $ map snd ls
+        compararPorPrimero = compare `on` fst
 
 knn :: Int -> Datos -> [Etiqueta] -> Medida -> Modelo
-knn k datos es dist pto = calcular_max_moda (take k (sort procesada))
-  where procesada = zip (map (\x  -> dist x pto) datos) es
+knn k datos etiquetas distancia punto = calcularEtiquetaModa kMasCercanas
+  where kMasCercanas = take k (sort distanciaPorEtiqueta)
+        distanciaPorEtiqueta = zip listaDistancias etiquetas
+        listaDistancias = map distanciasAlPunto datos
+        distanciasAlPunto = distancia punto
 
 accuracy :: [Etiqueta] -> [Etiqueta] -> Float
-accuracy xs ys = fromIntegral (apariciones True (zipWith (==) xs ys)) / genericLength xs
-
+accuracy xs ys = fromIntegral cantidadAciertos / genericLength xs
+  where cantidadAciertos = apariciones True aciertos
+        aciertos = zipWith (==) xs ys
+    
 separarDatos :: Datos -> [Etiqueta] -> Int -> Int -> (Datos, Datos, [Etiqueta], [Etiqueta])
-separarDatos ds es nparts part = (sinParticion part (ajustar ds),
-                                  soloParticion part (ajustar ds),
-                                  sinParticion part (ajustar es),
-                                  soloParticion part (ajustar es))
-	where ajustar = take (nparts * tampart)
-	      sinParticion p xs = (take (tampart * (p - 1)) xs) ++ (drop (tampart * p) xs)
-	      soloParticion p xs = take tampart (drop (tampart * (p - 1)) xs)
-	      tampart = length ds `div` nparts
+separarDatos ds es nparts part = (sinParticion part (ajustarTamanio ds),
+                                  soloParticion part (ajustarTamanio ds),
+                                  sinParticion part (ajustarTamanio es),
+                                  soloParticion part (ajustarTamanio es))
+	where ajustarTamanio = take (nparts * tamanioParticion)
+	      sinParticion p xs = (take (tamanioParticion * (p - 1)) xs) ++ (drop (tamanioParticion * p) xs)
+	      soloParticion p xs = take tamanioParticion (drop (tamanioParticion * (p - 1)) xs)
+	      tamanioParticion = length ds `div` nparts
 
 primero :: (a,b,c,d) -> a
 primero (x,y,z,w) = x
