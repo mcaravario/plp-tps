ae4ff24870718286a8ddfad012325e917296fd4c
mcaravario <martin.caravario@gmail.com>
2016-04-07 19:34:11 -0300

    Agregado test de extraerFeatures

--- tp1/src/Tests-alu.hs
+++ tp1/src/Tests-alu.hs
@@ -8,6 +8,7 @@ import Data.List
 -- evaluar main para correr todos los tests
 main = runTestTT allTests
 texto_a_normalizar = ["abc c abc abc d", "a b a b", "abc"]
+textos_a = ["b=a", "a = 2; a = 4", "C:/DOS C:/DOS/RUN RUN/DOS/RUN"]
 
 allTests = test [
 	"split"   ~: testsSplit,
@@ -15,8 +16,9 @@ allTests = test [
 	"longitudPromedioPalabras" ~: testLongitudPromedio,
 	"repeticionesPromedio" ~: testRepeticionesPromedio,
 	"frecuenciaTokens" ~: testFrecuenciaTokens,
-	"normalizarExtractor" ~: testNormalizarExtractor
-	]
+	"normalizarExtractor" ~: testNormalizarExtractor,
+	"extraerFeatures" ~: testExtraerFeatures
+  ]
 
 testsSplit = test [
 	split ',' ",PLP,"        ~?= ["PLP"],
@@ -57,3 +59,10 @@ testNormalizarExtractor = test [
 	map (normalizarExtractor texto_a_normalizar genericLength) ("aaaaaaaaaaaaaaaa":texto_a_normalizar) ~?= [16/15,1.0,7/15,0.2],
 	map (normalizarExtractor texto_a_normalizar repeticionesPromedio) ("a a a a":texto_a_normalizar) ~?= [2.0,5/6,1.0,0.5]
 	]
+
+testExtraerFeatures = test [
+    extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] textos_a ~?= [[0.33333334,0.6666667],[0.12962963,1.0],[1.0,0.6666667]],
+  extraerFeatures [] textos_a ~?= [[],[],[]],
+  extraerFeatures [genericLength] [] ~?= [],
+  extraerFeatures [genericLength] ["a","ab","abc"] ~?= [[1/3], [2/3], [3/3]]
+  ]
