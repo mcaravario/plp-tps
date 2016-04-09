6fe3a453344e860dde3cc408745750ef488b968f
Lucas Gabriel Vuotto <l.vuotto92@gmail.com>
2016-04-09 11:25:23 -0300

    Agregada mecanismo para test de punto flotante
    Permite comparar dos números reales por "casi iguales", lo cual es necesario,
    dado que los redondeos te cagan la vida. Por ende, dados x, y en R, x es casi
    igual a y si |x - y| < 0.0001
    
    Junto con esto vienen más pruebas para distancias
    
    Signed-off-by: Lucas Gabriel Vuotto <l.vuotto92@gmail.com>

--- tp1/src/Tests-alu.hs
+++ tp1/src/Tests-alu.hs
@@ -5,10 +5,29 @@ import Tp
 import Test.HUnit
 import Data.List
 
--- evaluar main para correr todos los tests
-main = runTestTT allTests
+-- para unless en assertAlmostEqual, modificado a partir de la definición de
+-- assertEqual, sacada de https://hackage.haskell.org/package/HUnit
+import Control.Monad
+
 texto_a_normalizar = ["abc c abc abc d", "a b a b", "abc"]
 textos_a = ["b=a", "a = 2; a = 4", "C:/DOS C:/DOS/RUN RUN/DOS/RUN"]
+
+almostEq :: Float -> Float -> Bool
+almostEq x y = abs (x - y) < 0.0001
+
+assertAlmostEqual :: String -> Float -> Float -> Assertion
+assertAlmostEqual preface expected actual =
+	unless (almostEq actual expected) (assertFailure msg)
+	where msg = (if null preface then "" else preface ++ "\n") ++
+	            "expected: " ++ show expected ++ "\n but got: " ++ show actual
+
+infix 1 ||~=
+(||~=) :: Float -> Float -> Assertion
+actual ||~= expected = assertAlmostEqual "" expected actual
+
+-- evaluar main para correr todos los tests
+main = runTestTT allTests
+
 
 allTests = test [
 	"split"                    ~: testsSplit,
@@ -71,13 +90,17 @@ testExtraerFeatures = test [
 	]
 
 testDistEuclideana = test [
-	distEuclideana [1.0, 0.75, 0.8125] [0.75, 1.0, 0.5]     ~?= 0.47186464,
-	distEuclideana [-5.0, -2.5, -0.1] [-2.3, 0, 3.4]        ~?= 5.078386,
-	distEuclideana [-3.0, -2.5] [-2.3, 0.3]                 ~?= 2.886174,
+	distEuclideana [1.0, 0.75, 0.8125] [0.75, 1.0, 0.5]     ||~= 0.47186464,
+	distEuclideana [-5.0, -2.5, -0.1] [-2.3, 0, 3.4]        ||~= 5.078386,
+	distEuclideana [-3.0, -2.5] [-2.3, 0.3]                 ||~= 2.886174,
+	distEuclideana [2.0, 1.0] [1.0, 0.0]                    ||~= sqrt 2,
+	distEuclideana [0.5 * sqrt 2, -0.5 * sqrt 2] [0.0, 0.0] ||~= 1.0,
+	distEuclideana [3.0, 0.0] [0.0, 4.0]                    ||~= 5.0
 	]
 
 testDistCoseno = test [
-	distCoseno [0, 3, 4] [0, -3, -4]    ~?= -1.0,
-	distCoseno [-1.3, -10] [-0.1, -0.6] ~?= 0.99935657,
-	distCoseno [3.0, -2.1] [-5.2, 0.1]  ~?= -0.8301066
+	distCoseno [0, 3, 4] [0, -3, -4]           ||~= -1.0,
+	distCoseno [-1.3, -10] [-0.1, -0.6]        ||~= 0.99935657,
+	distCoseno [3.0, -2.1] [-5.2, 0.1]         ||~= -0.8301066,
+	distCoseno [-0.5, 0.5 * sqrt 3] [1.0, 0.0] ||~= -0.5
 	]
