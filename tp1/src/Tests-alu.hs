9b8f020634f8f774680b25c0058fc951231d66e8
Lucas Gabriel Vuotto <l.vuotto92@gmail.com>
2016-04-09 11:24:00 -0300

    Corrección de espacios en blanco
    Signed-off-by: Lucas Gabriel Vuotto <l.vuotto92@gmail.com>

--- tp1/src/Tests-alu.hs
+++ tp1/src/Tests-alu.hs
@@ -11,16 +11,16 @@ texto_a_normalizar = ["abc c abc abc d", "a b a b", "abc"]
 textos_a = ["b=a", "a = 2; a = 4", "C:/DOS C:/DOS/RUN RUN/DOS/RUN"]
 
 allTests = test [
-	"split"   ~: testsSplit,
-	"cuentas" ~: testsCuentas,
+	"split"                    ~: testsSplit,
+	"cuentas"                  ~: testsCuentas,
 	"longitudPromedioPalabras" ~: testLongitudPromedio,
-	"repeticionesPromedio" ~: testRepeticionesPromedio,
-	"frecuenciaTokens" ~: testFrecuenciaTokens,
-	"normalizarExtractor" ~: testNormalizarExtractor,
-	"extraerFeatures" ~: testExtraerFeatures,
-  "distanciaEuclideana" ~: testDistEuclideana,
-  "distanciaCoseno" ~: testDistCoseno
-  ]
+	"repeticionesPromedio"     ~: testRepeticionesPromedio,
+	"frecuenciaTokens"         ~: testFrecuenciaTokens,
+	"normalizarExtractor"      ~: testNormalizarExtractor,
+	"extraerFeatures"          ~: testExtraerFeatures,
+	"distanciaEuclideana"      ~: testDistEuclideana,
+	"distanciaCoseno"          ~: testDistCoseno
+	]
 
 testsSplit = test [
 	split ',' ",PLP,"        ~?= ["PLP"],
@@ -37,19 +37,19 @@ testsCuentas = test [
 	]
 
 testLongitudPromedio = test [
-	longitudPromedioPalabras ""           ~?= 0.0,
-	longitudPromedioPalabras "       "    ~?= 0.0,
-	longitudPromedioPalabras "a-bc,def.g" ~?= 10.0,
-	longitudPromedioPalabras "a-bc def.g" ~?= 4.5,
+	longitudPromedioPalabras ""                                ~?= 0.0,
+	longitudPromedioPalabras "       "                         ~?= 0.0,
+	longitudPromedioPalabras "a-bc,def.g"                      ~?= 10.0,
+	longitudPromedioPalabras "a-bc def.g"                      ~?= 4.5,
 	longitudPromedioPalabras "Este test tiene palabras $$++$$" ~?= 5.4
 	]
 
 testRepeticionesPromedio = test [
-	repeticionesPromedio "" ~?= 0.0,
-	repeticionesPromedio "    " ~?= 0.0,
-	repeticionesPromedio "a-a b-b a-a-a" ~?= 1.0,
+	repeticionesPromedio ""                                   ~?= 0.0,
+	repeticionesPromedio "    "                               ~?= 0.0,
+	repeticionesPromedio "a-a b-b a-a-a"                      ~?= 1.0,
 	repeticionesPromedio "lalala $$++$$ lalala lalala $$++$$" ~?= 2.5,
-	map repeticionesPromedio texto_a_normalizar ~?= [5/3,2.0,1.0]
+	map repeticionesPromedio texto_a_normalizar               ~?= [5/3, 2.0, 1.0]
 	]
 
 testFrecuenciaTokens = test [
@@ -58,25 +58,25 @@ testFrecuenciaTokens = test [
 	]
 
 testNormalizarExtractor = test [
-	map (normalizarExtractor texto_a_normalizar genericLength) ("aaaaaaaaaaaaaaaa":texto_a_normalizar) ~?= [16/15,1.0,7/15,0.2],
-	map (normalizarExtractor texto_a_normalizar repeticionesPromedio) ("a a a a":texto_a_normalizar) ~?= [2.0,5/6,1.0,0.5]
+	map (normalizarExtractor texto_a_normalizar genericLength) ("aaaaaaaaaaaaaaaa":texto_a_normalizar) ~?= [16/15, 1.0, 7/15, 0.2],
+	map (normalizarExtractor texto_a_normalizar repeticionesPromedio) ("a a a a":texto_a_normalizar) ~?= [2.0, 5/6, 1.0, 0.5]
 	]
 
 testExtraerFeatures = test [
-  extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] textos_a ~?= [[0.33333334,0.6666667],[0.12962963,1.0],[1.0,0.6666667]],
-  extraerFeatures [] textos_a ~?= [[],[],[]],
-  extraerFeatures [genericLength] [] ~?= [],
-  extraerFeatures [genericLength] ["a","ab","abc"] ~?= [[1/3], [2/3], [3/3]]
-  ]
+	extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] textos_a ~?= [[0.33333334, 0.6666667], [0.12962963, 1.0], [1.0, 0.6666667]],
+	extraerFeatures [] textos_a ~?= [[],[],[]],
+	extraerFeatures [genericLength] [] ~?= [],
+	extraerFeatures [genericLength] ["a","ab","abc"] ~?= [[1/3], [2/3], [3/3]]
+	]
 
 testDistEuclideana = test [
-  distEuclideana [1.0,0.75,0.8125] [0.75,1.0,0.5] ~?= 0.47186464,
-  distEuclideana [-5.0,-2.5,-0.1] [-2.3,0,3.4] ~?= 5.078386,
-  distEuclideana [-3.0,-2.5,-0.1] [-2.3,0.3]  ~?= 2.886174
-  ]
+	distEuclideana [1.0, 0.75, 0.8125] [0.75, 1.0, 0.5]     ~?= 0.47186464,
+	distEuclideana [-5.0, -2.5, -0.1] [-2.3, 0, 3.4]        ~?= 5.078386,
+	distEuclideana [-3.0, -2.5] [-2.3, 0.3]                 ~?= 2.886174,
+	]
 
 testDistCoseno = test [
-  distCoseno [0,3,4] [0,-3,-4] ~?= -1.0,
-  distCoseno [-1.3,-10] [-0.1, -0.6] ~?= 0.99935657,
-  distCoseno [3.0, -2.1] [-5.2, 0.1] ~?= -0.8301066
-  ]
+	distCoseno [0, 3, 4] [0, -3, -4]    ~?= -1.0,
+	distCoseno [-1.3, -10] [-0.1, -0.6] ~?= 0.99935657,
+	distCoseno [3.0, -2.1] [-5.2, 0.1]  ~?= -0.8301066
+	]
