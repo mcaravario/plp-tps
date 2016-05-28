328757bdcb55557c41e960583f19b359b504201e
Martin Caravario <macaravario@dc.uba.ar>
2016-05-28 15:14:44 -0300

    Subo ejercicios 1 y 2

--- tp2/src/esqueleto.pl
+++ tp2/src/esqueleto.pl
@@ -26,3 +26,25 @@ ej(1, [rombo, cuadrado, espacio, perro, cuadrado, sol, cuadrado]).
 ej(2, [rombo, cuadrado, espacio, perro, triangulo, sol, cuadrado]).
 
 ej(3, [rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, gato]).
+
+
+% Ejercicio 1
+% 
+% string_codes/2(?string, ?listaDeCods) necesita que alguno de los dos argumentos debe estar
+% instanciado.
+%
+% diccionario_lista(?L) es reversible, ya que si le pasas una palabra (como
+% lista de ascii), devolvera true si pertenece al diccionario cargado
+% previamente y false en caso contrario. En caso de pasarle un parametro sin
+% instanciar devolvera las palabras como lista de ascii. 
+
+diccionario_lista(L) :- diccionario(X), string_codes(X,L). 
+
+% Ejercicio 2
+% juntar_con(+Xs, ?J, ?R)
+juntar_con([J],_,J).
+juntar_con([H|T], J, R) :- append(H,[J],R1), juntar_con(T,J,R2), append(R1,R2,R).
+
+% Ejercicio 3
+% palabras(+S,?P)
+
